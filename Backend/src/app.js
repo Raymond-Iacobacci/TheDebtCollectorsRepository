b3b6fb5a7272184cae44ff1c2f5c5f45ba98a8d2
Ajay Talanki <ajay.talanki@gmail.com>
2024-02-14 22:34:40 -0800

    refactored pool.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const {pool, displayConnectionError, displayQueryError} = require('./pool.js')
 const sendEmail = require('./sendEmail');
 const authRouter = require('./auth');
 require('dotenv').config({ path: '../.env' });
@@ -10,25 +10,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRouter);
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    connectionLimit : 1000
-});
-
-const displayConnectionError = (err, res) => {
-    console.error('Error getting database connection:', err);
-    res.status(500).send('Error getting database connection');
-}
-
-const displayQueryError = (queryErr, res) => {
-    console.error('Error executing query:', queryErr);
-    res.status(500).send('Error executing query');
-}
 
 const showEntries = (tableName, res) => {
   pool.getConnection((err, connection) => {
--- Backend/src/auth.js
+++ Backend/src/auth.js
@@ -3,8 +3,8 @@ const session = require('express-session');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const passport = require('passport');
 const crypto = require('crypto');
+const {pool} = require('./pool')
 require('dotenv').config({ path: '../.env' });
-const mysql = require('mysql');
 
 const authRouter = express.Router();
 
@@ -13,15 +13,6 @@ authRouter.use(express.urlencoded({ extended: true }));
 authRouter.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: false }));
 authRouter.use(passport.initialize());
 authRouter.use(passport.session());
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  connectionLimit : 1000
-});
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
@@ -45,7 +36,6 @@ function(request, accessToken, refreshToken, profile, done) {
       }
   
       if (results.length > 0) {
-        console.log(results);
         console.log("USER WAS FOUND IN THE DATABASE!");
         connection.release();
         return done(null, results[0]);
@@ -59,12 +49,10 @@ function(request, accessToken, refreshToken, profile, done) {
         }
 
         const newUser = {
-          id: profile.id,
+          id: results.insertId,
           email: profile.email
         };
-        
-        newUser.id = results.insertId; 
-        console.log(user);
+
         console.log("USER WAS ADDED TO THE DATABASE!");
         return done(null, newUser);
       });
