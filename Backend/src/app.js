22881526af98c5d56512c9dd782300f5ca5a4140
Ajay Talanki <ajay.talanki@gmail.com>
2024-03-04 11:35:55 -0800

    removed outdated REST API calls

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,43 +16,6 @@ app.use(cors());
 app.use('/auth', authRouter);
 app.use('/requests', requestsRouter); 
 
-const addEntries = (tableName, req, res) => {
-  pool.getConnection((err, connection) => {
-      if (err) {
-        console.error('Error getting database connection:', err);
-        res.status(500).send('Error getting database connection');
-        return;
-      }
-
-      let query = "";
-      let values = []
-
-      if(tableName == "tenants"){
-        const { userID, username, password, unit, email } = req.body;
-        values = [userID, username, password, email, unit];
-        query = 'INSERT INTO tenants VALUES (?, ?, ?, ?, ?);'
-      } else if(tableName == "managers"){
-        const { managerID, username, password, email } = req.body;
-        values = [managerID, username, password, email];
-        query = 'INSERT INTO managers VALUES (?, ?, ?, ?);'
-      } else if(tableName == "requests"){
-        const { requestID, summary, datePosted, unit, managerID } = req.body;
-        values = [requestID, summary, datePosted, unit, managerID];
-        query = 'INSERT INTO requests VALUES (?, ?, ?, ?, ?);'
-      }
-
-      connection.query(query, values, (queryErr, results) => {
-          connection.release();
-          if (queryErr) {
-              console.error('Error executing query:', queryErr);
-              res.status(500).send('Error executing query');
-              return;
-          }
-          res.json(results);
-      });
-  });
-}
-
 app.get('/send-email', (req, res) => {
   const emailToken = crypto.randomBytes(20).toString('hex');
   const oauthLink = `https://thedebtcollectorstest-kxfzqfz2rq-uc.a.run.app/auth/login?oauth_token=${emailToken}`;
@@ -66,26 +29,6 @@ app.get('/send-email', (req, res) => {
           res.json({ message: 'Email sent successfully' });
       }
   });
-});
-
-app.get('/show-tenants', (req, res) => {
-  selectQuery('tenants', res);
-});
-
-app.get('/show-managers', (req, res) => {
-  selectQuery('managers', res);
-});
-
-app.get('/show-requests', (req, res) => {
-  selectQuery('requests', res);
-});
-  
-app.post('/add-tenant', (req, res) => {
-  addEntries('tenants', req, res);
-});
-
-app.post('/add-manager', (req, res) => {
-  addEntries('managers', req, res);
 });
 
 app.listen(PORT, () => {
