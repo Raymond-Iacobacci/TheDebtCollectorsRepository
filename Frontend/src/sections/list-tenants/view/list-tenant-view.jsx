323c57908b464c44d0b4f0549f37b76034012a7d
Raymond_Iacobacci <rliacobacci@ucdavis.edu>
2024-04-11 22:39:05 -0700

    UI changes

--- Frontend/src/sections/list-tenants/view/list-tenant-view.jsx
+++ Frontend/src/sections/list-tenants/view/list-tenant-view.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'; 
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Dialog from '@mui/material/Dialog';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -16,54 +16,80 @@ import { TextField } from '@material-ui/core';
 
 // import Typography from '@mui/material/Typography';
 
-export default function ListTenantView({managerID}){
+export default function ListTenantView({ managerID }) {
     const [open, setOpen] = useState(false);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [address, setAddress] = useState("");
-    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [email, setEmail] = useState('');
 
-
-    const openPopup = () =>{
+    const openPopup = () => {
         setOpen(true);
-    }
+    };
     const handleClose = () => {
         setOpen(false); // Close the dialog
-    }
-    const createTenant = async() =>{
-        await fetch(`${import.meta.env.VITE_MIDDLEWARE_URL}/manager/create-tenant?manager-id=${managerID}`, {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              firstName: `${firstName}`,
-              lastName: `${lastName}`,
-              email: `${email}`,
-              address: `${address}`
-            })
-        });
+    };
+    const createTenant = async () => {
+        await fetch(
+            `${import.meta.env.VITE_MIDDLEWARE_URL}/manager/create-tenant?manager-id=${managerID}`,
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: `${firstName}`,
+                    lastName: `${lastName}`,
+                    email: `${email}`,
+                    address: `${address}`,
+                }),
+            }
+        );
         handleClose();
-    }
+    };
 
-    return(
+    return (
         <Container>
             <Button component="label" variant="contained" onClick={openPopup}>
-                  Create Tenant
-            </Button>   
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle id="alert-dialog-title">New Tenant</DialogTitle>
-                <Grid container>
+                Create Tenant
+            </Button>
+            <Dialog open={open} onClose={handleClose} sx={{ textAlign: 'center'}} >
+                <Grid container justifyContent="center">
                     <Grid>
-                        <Box>
-                            <TextField value={firstName} label="First Name" onChange={(e) => setFirstName(e.target.value)} /> 
-                            <TextField value={lastName} label="Last Name" onChange={(e) => setLastName(e.target.value)} />
-                            <TextField value={email} label ="Email" onChange={(e) => setEmail(e.target.value)}/>
-                            <TextField value={address} label="Address" onChange={(e) => setAddress(e.target.value)}/>
-                            <Button component="label" variant="contained" onClick={createTenant}>
-                               Submit
-                            </Button>
+                        <Box sx={{ padding: '20px' }}>
+                            <Grid item xs={12}>
+                                <DialogTitle id="alert-dialog-title" sx={{ textAlign: 'center', borderBottom: '2px solid #000000' }}>New Tenant</DialogTitle>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    value={firstName}
+                                    label="First Name"
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    value={lastName}
+                                    label="Last Name"
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField value={email} label="Email" onChange={(e) => setEmail(e.target.value)} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    value={address}
+                                    label="Address"
+                                    onChange={(e) => setAddress(e.target.value)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sx={{ marginTop: '20px' }}>
+                                <Button component="label" variant="contained" onClick={createTenant}>
+                                    Submit
+                                </Button>
+                            </Grid>
                         </Box>
                     </Grid>
                 </Grid>
