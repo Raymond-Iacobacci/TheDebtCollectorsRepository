f414cff3caf4751f53eee894b9b0ef19a5fdb687
Thaddeus Dziura <49488898+thaddyD@users.noreply.github.com>
2024-05-10 15:40:14 -0700

    Update report-view.jsx

--- Frontend/src/sections/report/report-view.jsx
+++ Frontend/src/sections/report/report-view.jsx
@@ -2,14 +2,20 @@ import { useState, useEffect } from 'react';
 
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import Popover from '@mui/material/Popover';
 import Divider from '@mui/material/Divider';
+import MenuItem from '@mui/material/MenuItem';
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 
 import { usePathname } from 'src/routes/hooks';
 
 import { fCurrency } from 'src/utils/format-number';
+
+import Label from 'src/components/label';
+import Iconify from 'src/components/iconify';
 
 // ----------------------------------------------------------------------
 
@@ -20,44 +26,56 @@ export default function ReportView() {
   const [loading, setLoading] = useState(true);
   const [errorMsg, setError] = useState('');
 
-  const [paid, setPaid] = useState({});
-  const [expenses, setExpenses] = useState({});
+  const [paid, setPaid] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+  const [timePeriod, setTimePeriod] = useState('monthly');
+  const [timePopover, setTimePopover] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        await fetch(`${import.meta.env.VITE_MIDDLEWARE_URL}/manager/get-report?manager-id=${uuid}`)
+        await fetch(
+          `${
+            import.meta.env.VITE_MIDDLEWARE_REPORT_URL
+          }/manager/get-report?manager-id=${uuid}&schedule=${timePeriod}`
+        )
           .then((res) => res.json())
           .then((data) => {
-            console.log(data);
-            console.log(data.overdueRent);
-            Object.entries(data).forEach(([key, value]) => {
-              let category;
-              if (key.includes('income')) {
-                category = 'income';
-              } else if (key.includes('expense')) {
-                category = 'expenses';
-              } else {
-                category = 'misc';
+            let finalPaid = [];
+            let finalExpenses = [];
+            for( let i = 0; i < data.length; i += 1 ) {
+              if( (lastFourTimePeriods(i) && timePeriod === 'monthly') || timePeriod !== 'monthly' ) {
+                const period = data[i];
+                const parsedIncome = {};
+                const parsedExpenses = {};
+                Object.entries(period).forEach(([key, value]) => {
+                  let category;
+                  if (key.includes('income')) {
+                    category = 'income';
+                  } else if (key.includes('expense')) {
+                    category = 'expenses';
+                  } else {
+                    category = 'misc';
+                  }
+                  switch (category) {
+                    case 'income':
+                      parsedIncome[key.substring(7)] = value;
+                      break;
+                    case 'expenses':
+                      parsedExpenses[key.substring(9)] = value;
+                      break;
+                    default:
+                      break;
+                  }
+                });
+                finalPaid = [...finalPaid, parsedIncome];
+                finalExpenses = [...finalExpenses, parsedExpenses];
               }
-              switch (category) {
-                case 'income':
-                  setPaid((prevState) => ({
-                    ...prevState,
-                    [key.replace(`${category}_`, '')]: value.toString(),
-                  }));
-                  break;
-                case 'expenses':
-                  setExpenses((prevState) => ({
-                    ...prevState,
-                    [key.replace(`${category}_`, '')]: value.toString(),
-                  }));
-                  break;
-                default:
-                  break;
-              }
-            });
+            }
+            setPaid(finalPaid);
+            setExpenses(finalExpenses);
           });
         setLoading(false);
       } catch (error) {
@@ -66,26 +84,64 @@ export default function ReportView() {
       }
     };
     fetchData();
-  }, [uuid]);
+  }, [uuid, timePeriod]);
+
+  const lastFourTimePeriods = (index) => {
+    const cmonth = new Date().getMonth();
+    if( index <= cmonth && cmonth-4 < index) {
+      return true;
+    }
+    return false;
+  };
 
   const renderItems = (map, label) => (
     <>
       <Typography gutterBottom variant="h5" sx={{ margin: '8px' }}>
         {label}
       </Typography>
-      {renderLegend('Weekly')}
-      {Object.entries(map).map(([key, value]) => (
-        <Grid container key={key}>
-          <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
-            <Typography variant="body1">{key.charAt(0).toUpperCase() + key.slice(1)}</Typography>
-          </Grid>
-          <Grid item xs={6} sx={{ paddingRight: '32px' }}>
-            <Typography align="right" variant="body1">
-              {fCurrency(value)}
-            </Typography>
-          </Grid>
-        </Grid>
-      ))}
+      {renderLegend(timePeriod)}
+      {map.length !== 0 ? (
+        Object.entries(map[0]).map(([key, value]) => (
+          <Stack direction="row" alignItems="center" justifyContent="space-between">
+            <Grid item xs={3} sx={{ paddingLeft: '16px' }}>
+              <Typography variant="body1">{key.charAt(0).toUpperCase() + key.slice(1)}</Typography>
+            </Grid>
+            {timePeriod === 'monthly' ? (
+              Object.entries(map).map(([k, v]) => (
+                <Grid xs={2}>
+                  <Typography align="right" variant="body1">
+                    {fCurrency(v[key].toString())}
+                  </Typography>
+                </Grid>
+              )
+            )) : (
+              <div />
+            )}
+            {timePeriod === 'quarterly' ? (
+              Object.entries(map).map(([k, v]) => (
+                <Grid xs={2}>
+                  <Typography align="right" variant="body1">
+                    {fCurrency(v[key].toString())}
+                  </Typography>
+                </Grid>
+              ))
+            ) : (
+              <div />
+            )}
+            {timePeriod === 'yearly' ? (
+              <Grid item xs={6}>
+                <Typography align="right" variant="body1">
+                  {fCurrency(value)}
+                </Typography>
+              </Grid>
+            ) : (
+              <div />
+            )}
+          </Stack>
+        ))
+      ) : (
+        <div />
+      )}
     </>
   );
 
@@ -102,26 +158,30 @@ export default function ReportView() {
     </Grid>
   );
 
-  const renderColumnTotals = (map) => (
-    <>
-      <Divider sx={{ borderStyle: 'dashed', marginTop: '12px', marginRight: '20px', marginLeft: '20px' }} />
-      <Grid container sx={{ marginTop: '8px' }}>
-        <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
-          <Typography variant="subtitle1">Total</Typography>
-        </Grid>
-        <Grid item xs={6} sx={{ paddingRight: '32px' }}>
-          <Typography align="right" variant="subtitle1">
-            {fCurrency(calculateOverallTotal(map))}
-          </Typography>
-        </Grid>
-      </Grid>
-    </>
-  );
+  // const renderColumnTotals = (map) => (
+  //   <>
+  //     <Divider
+  //       sx={{ borderStyle: 'dashed', marginTop: '12px', marginRight: '20px', marginLeft: '20px' }}
+  //     />
+  //     <Grid container sx={{ marginTop: '8px' }}>
+  //       <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
+  //         <Typography variant="subtitle1">Total</Typography>
+  //       </Grid>
+  //       <Grid item xs={6} sx={{ paddingRight: '32px' }}>
+  //         <Typography align="right" variant="subtitle1">
+  //           {fCurrency(calculateOverallTotal(map))}
+  //         </Typography>
+  //       </Grid>
+  //     </Grid>
+  //   </>
+  // );
 
-  const calculateOverallTotal = (category) => {
+  const calculateOverallTotal = (data) => {
     let total = 0;
-    Object.values(category).forEach((amount) => {
-      total += parseInt(amount, 10);
+    data.forEach((period) => {
+      Object.entries(period).forEach(([key, value]) => {
+        total += value;
+      });
     });
     return total.toString();
   };
@@ -146,57 +206,74 @@ export default function ReportView() {
 
   const renderLegend = (interval) => {
     switch (interval) {
-      case 'Daily':
+      case 'monthly':
         return (
-          <Grid container sx={{ marginBottom: '6px' }}>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" >
             <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
               <Typography variant="subtitle1">Category</Typography>
             </Grid>
-            <Grid item xs={6} sx={{ paddingRight: '32px' }}>
+            <Grid item xs={3}>
               <Typography align="right" variant="subtitle1">
-                Today
+                February
               </Typography>
             </Grid>
-          </Grid>
+            <Grid item xs={3}>
+              <Typography align="right" variant="subtitle1">
+                March
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography align="right" variant="subtitle1">
+                April
+              </Typography>
+            </Grid>
+            <Grid item xs={3} sx={{ paddingRight: '32px' }}>
+              <Typography align="right" variant="subtitle1">
+                May
+              </Typography>
+            </Grid>
+          </Stack>
         );
-      case 'Weekly':
+      case 'quarterly':
         return (
-          <Grid container sx={{ marginBottom: '6px' }}>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" >
             <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
               <Typography variant="subtitle1">Category</Typography>
             </Grid>
-            <Grid item xs={6} sx={{ paddingRight: '32px' }}>
+            <Grid item xs={3}>
               <Typography align="right" variant="subtitle1">
-                This Week
+                Q1
               </Typography>
             </Grid>
-          </Grid>
+            <Grid item xs={3}>
+              <Typography align="right" variant="subtitle1">
+                Q2
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography align="right" variant="subtitle1">
+                Q3
+              </Typography>
+            </Grid>
+            <Grid item xs={3} sx={{ paddingRight: '32px' }}>
+              <Typography align="right" variant="subtitle1">
+                Q4
+              </Typography>
+            </Grid>
+          </Stack>
         );
-      case 'Monthly':
+      case 'yearly':
         return (
-          <Grid container sx={{ marginBottom: '6px' }}>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" >
             <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
               <Typography variant="subtitle1">Category</Typography>
             </Grid>
-            <Grid item xs={6} sx={{ paddingRight: '32px' }}>
+            <Grid item xs={3} sx={{ paddingRight: '16px' }}>
               <Typography align="right" variant="subtitle1">
-                This Month
+                2024
               </Typography>
             </Grid>
-          </Grid>
-        );
-      case 'Yearly':
-        return (
-          <Grid container sx={{ marginBottom: '6px' }}>
-            <Grid item xs={6} sx={{ paddingLeft: '16px' }}>
-              <Typography variant="subtitle1">Category</Typography>
-            </Grid>
-            <Grid item xs={6} sx={{ paddingRight: '32px' }}>
-              <Typography align="right" variant="subtitle1">
-                This Year
-              </Typography>
-            </Grid>
-          </Grid>
+          </Stack>
         );
       default:
         return (
@@ -214,24 +291,82 @@ export default function ReportView() {
     }
   };
 
+  const handleOpenTimePopover = (event) => {
+    setTimePopover(event.currentTarget);
+  };
+
+  const handleCloseTimePopover = (event) => {
+    setTimePopover(null);
+  };
+
+  const handleEditTimeToMonthly = async (event) => {
+    setPaid([]);
+    setExpenses([]);
+    setTimePeriod('monthly');
+    setTimePopover(null);
+  };
+
+  const handleEditTimeToQuarterly = async (event) => {
+    setPaid([]);
+    setExpenses([]);
+    setTimePeriod('quarterly');
+    setTimePopover(null);
+  };
+
+  const handleEditTimeToYearly = async (event) => {
+    setPaid([]);
+    setExpenses([]);
+    setTimePeriod('yearly');
+    setTimePopover(null);
+  };
+
   return (
     <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Report</Typography>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          spacing={3}
+          sx={{ p: 0, pr: 0 }}
+        >
+          <Label color="primary">{timePeriod}</Label>
+          <IconButton onClick={handleOpenTimePopover}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+          <Popover
+            open={!!timePopover}
+            anchorEl={timePopover}
+            onClose={handleCloseTimePopover}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            <MenuItem onClick={handleEditTimeToMonthly} sx={{ pt: 1 }}>
+              <Label color="primary">Monthly</Label>
+            </MenuItem>
+            <MenuItem onClick={handleEditTimeToQuarterly}>
+              <Label color="primary">Quarterly</Label>
+            </MenuItem>
+            <MenuItem onClick={handleEditTimeToYearly} sx={{ pb: 1 }}>
+              <Label color="primary">Yearly</Label>
+            </MenuItem>
+          </Popover>
+        </Stack>
       </Stack>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={8}>
+        <Grid item xs={12} sm={12} md={8}>
           <Card sx={{ padding: '16px' }}>
             {renderItems(expenses, 'Expenses')}
-            {renderColumnTotals(expenses)}
+            {/* {renderColumnTotals(expenses)} */}
           </Card>
           <Card sx={{ padding: '16px', marginTop: '25px' }}>
             {renderItems(paid, 'Income')}
-            {renderColumnTotals(paid)}
+            {/* {renderColumnTotals(paid)} */}
           </Card>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
+        <Grid item xs={12} sm={12} md={4}>
           <Card sx={{ padding: '16px' }}>
             <Typography gutterBottom variant="h5" sx={{ margin: '8px' }}>
               Totals
