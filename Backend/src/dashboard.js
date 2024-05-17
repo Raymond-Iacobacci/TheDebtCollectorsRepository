beb255053aae2503ab079a0bddbf1188eee9131d
Ajay Talanki <ajay.talanki@gmail.com>
2024-05-17 16:54:47 -0700

    implemented num rent payments

--- Backend/src/dashboard.js
+++ Backend/src/dashboard.js
@@ -26,4 +26,31 @@ dashBoardRouter.get('/get-number-of-unresolved-requests', async (req, res) => {
   }
 });
 
+dashBoardRouter.get('/get-number-of-rent-payments', async (req, res) => {
+  try{
+    const managerID = '0x' + req.query['manager-id'];
+    const currentDate = new Date();
+    const startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const formattedStartDate = startDate.toISOString().slice(0, 10);
+    const formattedEndDate = currentDate.toISOString().slice(0, 10);
+
+    const query = `SELECT COUNT(*) AS count
+    FROM (
+      SELECT type, description, amount, time, paidAmount
+      FROM paymentsLedger p
+      INNER JOIN (
+        SELECT tenantID
+        FROM tenants
+        WHERE managerID = ${managerID}
+      ) AS t ON t.tenantID = p.tenantID
+    ) AS final
+    WHERE final.type = 'Charge' AND final.paidAmount=0 AND final.description = 'Rent' AND final.time >= '${formattedStartDate}' AND final.time <= '${formattedEndDate}'`;
+
+    const results = await executeQuery(query);
+    res.send(results[0]);
+  } catch (error) {
+    res.status(500).json({ error: `error getting number of rent payments with error: ${error}` });
+  }
+});
+
 module.exports = dashBoardRouter;
